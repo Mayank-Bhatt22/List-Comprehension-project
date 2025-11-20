5b116798ec1c61a299bfcded910f4854decf6c98
Mayank Bhatt <bhattmayank99711@gmail.com>
2025-11-21 00:45:59 +0530

    Add employee data to data.py

---
+++ data.py
@@ -0,0 +1,17 @@
+employees = [
+    {"id": 101, "name": "Amit", "dept": "HR", "salary": 45000, "city": "Delhi", "exp": 2},
+    {"id": 102, "name": "Riya", "dept": "Finance", "salary": 60000, "city": "Mumbai", "exp": 4},
+    {"id": 103, "name": "Suresh", "dept": "IT", "salary": 75000, "city": "Bangalore", "exp": 5},
+    {"id": 104, "name": "Meena", "dept": "IT", "salary": 82000, "city": "Delhi", "exp": 7},
+    {"id": 105, "name": "Vikas", "dept": "HR", "salary": 50000, "city": "Chennai", "exp": 3},
+    {"id": 106, "name": "Tina", "dept": "Finance", "salary": 65000, "city": "Delhi", "exp": 6},
+    {"id": 107, "name": "Rohan", "dept": "Sales", "salary": 40000, "city": "Mumbai", "exp": 2},
+    {"id": 108, "name": "Neha", "dept": "Sales", "salary": 55000, "city": "Pune", "exp": 4},
+    {"id": 109, "name": "Karan", "dept": "IT", "salary": 90000, "city": "Bangalore", "exp": 8},
+    {"id": 110, "name": "Pooja", "dept": "HR", "salary": 47000, "city": "Delhi", "exp": 2},
+    {"id": 111, "name": "Rahul", "dept": "Finance", "salary": 75000, "city": "Mumbai", "exp": 5},
+    {"id": 112, "name": "Sneha", "dept": "Sales", "salary": 58000, "city": "Delhi", "exp": 4},
+    {"id": 113, "name": "Manish", "dept": "IT", "salary": 72000, "city": "Pune", "exp": 6},
+    {"id": 114, "name": "Harsh", "dept": "HR", "salary": 52000, "city": "Bangalore", "exp": 3},
+    {"id": 115, "name": "Geeta", "dept": "Finance", "salary": 69000, "city": "Chennai", "exp": 4}
+]
